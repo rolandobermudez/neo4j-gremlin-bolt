0454e2e37976b4f99b99b670266801697b251eeb
Rogelio J. Baucells <rogelio.javier@baucells.net>
2016-06-21 19:21:38 -0400

    build updates

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    # feedback
+    echo "Processing signing key..."
+    # decrypt signing key
     openssl aes-256-cbc -K $encrypted_5efcbdf945b5_key -iv $encrypted_5efcbdf945b5_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    # import signing key
     gpg --fast-import .travis/signingkey.asc
 fi
 
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    # feedback
+    echo "Deploying artifacts..."
+    # deploy
     mvn deploy -P sign,build-extras --settings .travis/mvnsettings.xml
 fi
 
